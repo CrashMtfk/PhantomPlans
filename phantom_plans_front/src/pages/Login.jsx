75fea5b4ea2807360d631ac0ac6bc7d7ed4ed5e2
Rus-Claudiu <90281742+CrashMtfk@users.noreply.github.com>
2023-04-16 20:34:34 +0300

    finished login design

--- phantom_plans_front/src/pages/Login.jsx
+++ phantom_plans_front/src/pages/Login.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import Navbar from '../layout/Navbar'
 import Footer from '../layout/Footer'
+import loginLogo from '../assets/sign_login_logo.svg'
+import '../styling/login.css'
+import {Link} from 'react-router-dom'
 
 export default function LogIn() {
   return (
     <div className='login-container'>
-      <Navbar/>
-      <h1>Log in</h1>
-      <Footer/>
+      <Navbar />
+      <div className="content-container w-25 mx-auto d-flex flex-column align-items-center justify-content-center">
+        <div className="logo-container mt-5 mb-5">
+          <img src={loginLogo} alt="" />
+        </div>
+        <div className="form-container w-50">
+          <form action="" className='d-flex flex-column'>
+            <div className="mb-4">
+              <input type="text" className='form-control username-input' id="usernameInput" placeholder='Username' />
+            </div>
+            <div className="mb-4">
+              <input type="password" className='form-control password-input' id="passwordInput" placeholder='Password' />
+            </div>
+            <button type="submit" className='btn wb-3 w-50 mx-auto text-light' id='login-btn'>Log In</button>
+          </form>
+          <div className="no-account-text mb-5 mt-5">
+            <p className='text-center account-text'>Don't have an account?<Link to={'/signup'} className='nav-link text-light'>Sign Up</Link></p>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
