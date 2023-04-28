5140e26b0fef78a6792945d47e610d3ad4490a9b
Rus-Claudiu <90281742+CrashMtfk@users.noreply.github.com>
2023-04-28 17:11:53 +0300

    tried some things out

---
+++ phantom_plans_front/src/layout/SidebarNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarNav() {
+    return (
+        <div>
+            <h1>ma iau si ma arunc imediat</h1>
+        </div>
+    )
+}
+
+export default SidebarNav
--- phantom_plans_front/src/pages/Profile.jsx
+++ phantom_plans_front/src/pages/Profile.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SidebarNav from '../layout/SidebarNav'
 
 function Profile({user}) {
   return (
-    <div>Profile</div>
+    <div>
+        <SidebarNav />
+    </div>
   )
 }
 
