bf757368f253fddbe91743f1e059a9011dcfbc61
Rus-Claudiu <90281742+CrashMtfk@users.noreply.github.com>
2023-04-30 22:02:27 +0300

    Implemented sidebar and almost finished design

--- phantom_plans_front/src/pages/Tasks.jsx
+++ phantom_plans_front/src/pages/Tasks.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import SidebarNav from '../layout/SidebarNav';
 
 function Tasks({ user }) {
 
@@ -8,34 +9,35 @@ function Tasks({ user }) {
   const displayTasks = async () => {
     const accessToken = localStorage.getItem('token');
     await axios.get("http://localhost:5000/tasks", {
-        headers: {
-          Authorization: 'Bearer ' + accessToken
-        },
-        params: {
-          userId: user.id
-        }
-      }).then(result => {
-        if(result){
-          console.log('Successfull fetch!');
-          console.log(result.data);
-          setTaskList(result.data);
-        }else {
-          console.log("No data fetched!");
-        }
-      }).catch(err => console.log(err));
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      },
+      params: {
+        userId: user.id
+      }
+    }).then(result => {
+      if (result) {
+        console.log('Successfull fetch!');
+        console.log(result.data);
+        setTaskList(result.data);
+      } else {
+        console.log("No data fetched!");
+      }
+    }).catch(err => console.log(err));
   }
 
   return (
-    <div>
-      <h1>Hello + {user.username}</h1>
-      <button onClick={displayTasks}>Display Tasks</button>
-      {
-        taskList.map(task => {
-          return <h2 key={task._id}>
-            {task.title}
-          </h2>
-        })
-      }
+    <div style={{ display: 'flex' }}>
+      <SidebarNav user={user} />
+      <div className="tasks-content w-100">
+        <div className="header-content">
+          <div className="title-container">
+            <h2>Tasks</h2>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
